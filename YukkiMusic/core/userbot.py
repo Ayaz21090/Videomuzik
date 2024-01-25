4fdde73cdc7417c58eeecad48b5cf65b7c15c44d
Ayaz21090 <156663604+Ayaz21090@users.noreply.github.com>
2024-01-25 21:21:59 +0300

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -63,8 +63,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.two.join_chat("@kargasabirligitr")
-                await self.two.join_chat("@kargasabirligitr")  
+                await self.two.join_chat("")
+                await self.two.join_chat("")  
             except:
                 pass
             assistants.append(1)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("@kargasabirligitr")
-                await self.two.join_chat("@kargasabirligitr")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
             except:
                 pass
             assistants.append(2)
@@ -123,8 +123,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.two.join_chat("@kargasabirligitr")
-                await self.two.join_chat("@kargasabirligitr")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
             except:
                 pass
             assistants.append(3)
@@ -153,8 +153,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.two.join_chat("@kargasabirligitr")
-                await self.two.join_chat("@kargasabirligitr")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
             except:
                 pass
             assistants.append(4)
@@ -183,8 +183,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.two.join_chat("@kargasabirligitr")
-                await self.two.join_chat("@kargasabirligitr")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
             except:
                 pass
             assistants.append(5)
